9e4f0f25bcbaa233b66441891415a57d3e4a81fa
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-10-07 20:47:54 -0500

    Compojure routes

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.2.0"]]
+                 [ring "1.2.0"]
+                 [compojure "1.1.5"]]
   :main mt.core)
--- src/mt/server.clj
+++ src/mt/server.clj
@@ -1,14 +1,23 @@
 (ns mt.server
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :refer [defroutes GET]]
+            [ring.middleware.reload :as reload]))
 
 (def port 8080)
+
+(def home-template (slurp "./src/mt/templates/home.html"))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "<h1>test</h1>"})
+   :body home-template})
+
+
+(defroutes app-routes
+  (-> (GET "/" request (handler request))
+      (reload/wrap-reload)))
 
 (defn start []
   (println "starting server...")
-  (jetty/run-jetty (var handler)
+  (jetty/run-jetty app-routes
                    {:port port :join? false}))
